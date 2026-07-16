872196182139ad1f0d5a9f23f0c517b0f0b1b5c1
uppiyxdxv <upendrapallolla@gmail.com>
2026-07-17 01:21:13 +0530

    Deploy frontend to GitHub Pages

--- src/main/java/com/platform/repository/QuizRepository.java
+++
@@ -1,9 +0,0 @@
-package com.platform.repository;
-
-import com.platform.model.Quiz;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface QuizRepository extends JpaRepository<Quiz, String> {
-    List<Quiz> findByDomain(String domain);
-}
